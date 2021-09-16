919a33deb9939b157453d0d3e07892e42aa8ad3d
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2021-09-16 13:30:19 -0700

    created json

--- spotpytest.py
+++ spotpytest.py
@@ -1,16 +1,20 @@
 # %%
-from json.decoder import JSONDecodeError
-import spotipy.util as util
-import webbrowser
+import collections
 import json
-import sys
 import os
-from spotipy.oauth2 import SpotifyOAuth
-from spotipy.oauth2 import SpotifyClientCredentials
-import spotipy
 import pprint
-import pandas
+import sys
+import webbrowser
+from json.decoder import JSONDecodeError
+
 import matplotlib.pyplot as plt
+import pandas
+import simplejson
+import spotipy
+import spotipy.util as util
+import yaml
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+
 CLIENT_ID = '66da3fd147fb4433bf1a67a444f5d57e'
 CLIENT_SECRET = '1ca107b9f0114b00a43e7a81c44630e9'
 REDIRECT_URI = 'http://localhost:8080'
@@ -20,36 +24,43 @@ scope = "user-library-read"
 
 # USER ID: 12151542744
 
-#pretty printer
+# pretty printer
 pp = pprint.PrettyPrinter()
 # SPOTIFY OBJECT  sp
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                     client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=scope))
+splibrary = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                                      client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=scope))
 
-saved_songs = 0
+sptop = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                                  client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope='user-top-read'))
 
-def request_songs(loops,_offset):
+
+def request_songs(loops, _offset):
     for batch in range(1):
-        results = sp.current_user_saved_tracks(limit=10, offset=_offset*loops)
+        results = splibrary.current_user_saved_tracks(
+            limit=10, offset=_offset*loops)
         for idx, item in enumerate(results['items']):
             track = item['track']
-            print(idx, track['artists'][0]['name'], " – ", track['name'])
-        saved_songs = results
+            #print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+        songs = results['items']
+
+        return songs
 
 
-def song_analysis(songs):
-    data = []
-    for item in dict(saved_songs['items']):
-        track = item['track']
-        analysis = sp.audio_features(track['id'])
-        data.append()
+def user_top_artists():
+    scope = 'user-top-read'
+    results = sptop.current_user_top_tracks(limit=10, time_range="medium_term")
 
-    return data
+    with open('top_artists.txt', 'w') as outfile:
+        json.dump(results, outfile)
 
 
-def make_pandas():
-    df = pandas.DataFrame(song_analysis(request_songs(1,100)))
+def make_pandas(arg):
+    df = pandas.DataFrame(arg)
     pp.pprint(df)
 
-make_pandas()
+
+type(request_songs(2, 500))
+user_top_artists()
+
 # %%
