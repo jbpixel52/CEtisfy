aa7bd05551ce8e3a617a88fefdf0cb32e86de1a0
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2021-09-16 12:59:37 -0700

    changed to function

--- spotpytest.py
+++ spotpytest.py
@@ -20,37 +20,36 @@ scope = "user-library-read"
 
 # USER ID: 12151542744
 
+#pretty printer
+pp = pprint.PrettyPrinter()
 # SPOTIFY OBJECT  sp
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                      client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=scope))
-pp=pprint.PrettyPrinter()
-saved_songs=0
-for batch in range(1):
-    results = sp.current_user_saved_tracks(limit=20,offset=batch*50)
-    for idx, item in enumerate(results['items']):
-        track = item['track']
-        #print(idx, track['artists'][0]['name'], " – ", track['name'])
-    saved_songs =results
 
-for idx, item in enumerate(saved_songs['items']):
-        track = item['track']
-        print(idx, track['artists'][0]['name'], " – ", track['name'],track['id'])
-print('\n')       
-analysed_song= sp.audio_features("6NVB6W7G3svCKe5zB7kY8q")
-pp.pprint(analysed_song)
+saved_songs = 0
 
-def song_analysis():
+def request_songs(loops,_offset):
+    for batch in range(1):
+        results = sp.current_user_saved_tracks(limit=10, offset=_offset*loops)
+        for idx, item in enumerate(results['items']):
+            track = item['track']
+            print(idx, track['artists'][0]['name'], " – ", track['name'])
+        saved_songs = results
+
+
+def song_analysis(songs):
     data = []
-    for idx, item in enumerate(saved_songs['items']):
+    for item in dict(saved_songs['items']):
         track = item['track']
         analysis = sp.audio_features(track['id'])
-        data.append(analysis)
-        stringdata = str(track["artists"][0]['name'],str(' - '),str(track['name']))
-        metadata=[]
-        metadata.append(stringdata)        
-        
-        return [data,metadata]
-        
-dataset = song_analysis()
-df =pandas.DataFrame(dataset)
+        data.append()
+
+    return data
+
+
+def make_pandas():
+    df = pandas.DataFrame(song_analysis(request_songs(1,100)))
+    pp.pprint(df)
+
+make_pandas()
 # %%
