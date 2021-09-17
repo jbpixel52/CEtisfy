78805bf610ade06f976482d1ac54e93b83ea50fb
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2021-09-17 14:21:52 -0700

    created json with my (julio's) half of spotify saved songs

---
+++ MLtest/mltest.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+CLIENT_ID = '66da3fd147fb4433bf1a67a444f5d57e'
+CLIENT_SECRET = '1ca107b9f0114b00a43e7a81c44630e9'
+REDIRECT_URI = 'http://localhost:8080'
+
+
+scope = "user-library-read"
