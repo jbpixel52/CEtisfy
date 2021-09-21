671b253e1dbeff253ed83adffd7f1873b045c4c3
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2021-09-20 23:58:36 -0700

    cleaned unnecesary variables

--- MLtest/id_getters.py
+++ MLtest/id_getters.py
@@ -3,25 +3,21 @@
 """GETTERS FOR THE IDs OF ALL OBJECTS by SPOTIFY LINK
     """
 def get_playlists_id(url=input('Enter the spotify playlist link:')):
-    link = url.replace('https://open.spotify.com/playlist/', '')
-    return link.split('?')[0]
+    return url.replace('https://open.spotify.com/playlist/', '').split('?')[0]
+
+def get_album_id(url=input('Enter the spotify ALBUM link:')):
+    return url.replace('https://open.spotify.com/album/', '').split('?')[0]
 
 
-def get_album_id(url=input('Enter the spotify playlist link:')):
-    link = url.replace('https://open.spotify.com/album/', '')
-    return link.split('?')[0]
+def get_track_id(url=input('Enter the spotify TRACK link:')):
+    return url.replace('https://open.spotify.com/track/', '').split('?')[0]
 
 
-def get_track_id(url=input('Enter the spotify playlist link:')):
-    link = url.replace('https://open.spotify.com/track/', '')
-    return link.split('?')[0]
+def get_track_id(url=input('Enter the spotify ARTIST link:')):
+    return url.replace('https://open.spotify.com/artist/', '').split('?')[0]
 
 
-def get_track_id(url=input('Enter the spotify playlist link:')):
-    link = url.replace('https://open.spotify.com/artist/', '')
-    return link.split('?')[0]
+def get_user_id(url=input('Enter the spotify user ID link:')):
+    return url.replace('https://open.spotify.com/user/', '').split('?')[0]
 
-
-def get_user_id(url=input('Enter the spotify playlist link:')):
-    link = url.replace('https://open.spotify.com/user/', '')
-    return link.split('?')[0]
+get_playlists_id()
